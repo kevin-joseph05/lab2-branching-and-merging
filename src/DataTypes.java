6262f23919008fab777561835110dd1efc7236fd
ski-mask-the-slump-god <drakonfyre212@gmail.com>
2025-09-15 10:06:02 -0400

    task_1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,10 +1,10 @@
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+
     public static long sum(int[] numbers) {
         int s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
-            s += x;
+            s ++;
         }
         return s;
     }
