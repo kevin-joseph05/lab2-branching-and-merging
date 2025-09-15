67eb72aaa0ac2a3bec5e2d1031e4ae0fe98b4cba
ski-mask-the-slump-god <drakonfyre212@gmail.com>
2025-09-15 10:13:06 -0400

    changed datatypes

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,10 +1,10 @@
 public class DataTypes {
 
     public static long sum(int[] numbers) {
-        int s = 0; // variable to accumulate the sum in!
+        long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
-            s ++;
+            s +=x;
         }
         return s;
     }
